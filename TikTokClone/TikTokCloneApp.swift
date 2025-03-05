3c7242d6f06ffae5cdf62e120319ac9da9cec01f
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-03-05 12:38:31 +0000

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by TJ Gbadebo on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TikTokCloneTests/TikTokCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneTests.swift
+//  TikTokCloneTests
+//
+//  Created by TJ Gbadebo on 05/03/2025.
+//
+
+import Testing
+@testable import TikTokClone
+
+struct TikTokCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
